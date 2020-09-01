f1276a77b9036d5d1b9a66cc0f4347e1a4c1eaf9
shashi kanth <shashiknt.d@gmail.com>
2020-09-01 11:35:04 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// const circle = new mojs.Shape({
+//     shape:  			'circle',
+//     scale:   			{ 0 : 3 },
+//     left:         { 0 : '80%' },
+//     y:          { 0: '75    %' },
+//     fill:         { 'cyan': 'yellow' },
+//     radius:       25,
+    
+//     duration:     1000,
+//     repeat:       22,
+//   }).play();
+  
+  
+//   const rect = new mojs.Shape({
+//     shape:  			'rect',
+//     left:         '50%',
+//     fill:         'none',
+//     radius:       20,
+//     stroke:       { 'rgba(0,255,255, 1)' : 'magenta' },
+//     strokeWidth:  { 10: 0 },
+//     strokeDasharray: '100%',
+//     strokeDashoffset: { '-100%' : '100%' },
+//     angle:        { 0: 360 },
+    
+//     duration:     2000,
+//     repeat:       999,
+//   }).play();
+  
+//   const polygon = new mojs.Shape({
+//     shape:  			'polygon',
+//     points:       3,
+//     left:         '75%',
+//     //fill:         { 'deeppink' : '#00F87F' },
+//     x:            { 'rand(-100%, -2000%)' : 0  },
+//     angle:        { 0: 'rand(0, 360)' },
+//     radius:       50,
+  
+//     duration:     2000,
+//     repeat:       999,
+//   }).play();
+  
